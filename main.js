e71238d913794fbd551f4de04f4c3993b3646e79
roiwebco <91880878+NW360D@users.noreply.github.com>
2023-01-28 09:45:21 +0100

    33
    fullscreen app test

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
+//Register service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
     navigator.serviceWorker.register('./sw.js');
   });
 }
-// window.addEventListener('online',  updateIndicator);
-// window.addEventListener('offline', updateIndicator);
-
-// function updateIndicator() {
-//     if (navigator.onLine) {
-//         alert ('is online')
-//     }else{
-//         alert ('is offline')
-//     }
-// }
-
